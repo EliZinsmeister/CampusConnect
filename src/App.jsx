1043f92bd5133c6ca004502fca29cd3632f925f0
stevie-green <stevie-lynn_green@s.thevillageschool.com>
2025-09-17 12:53:28 -0500

    BUTTON

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+        <button>I'm a button</button>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
