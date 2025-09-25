efc28f8448973ee6f1e816a71889bc62d0d59c7c
stevie-green <stevie-lynn_green@s.thevillageschool.com>
2025-09-25 12:00:26 -0500

    sign up log in stuff

---
+++ src/assets/LogIn.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function logIn() {
+
+  return (
+    
+    <div>
+
+      <form name ="LogIn" action="LogInForm" method="POST">
+          <label>Username: </label><input type ="text" name ="username"/>
+          <br />
+          <label>Password: </label><input type ="text" name ="password"/>
+          <br />
+          <input type = "submit" value ="Log In"/>
+      </form>
+      <br />
+      <a href ="BackButton" className="abutton">
+          <button>Back</button>
+      </a>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/assets/SignUp.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function signUp() {
+
+  return (
+    <>
+     <body>
+
+        <form name ="SignUp" action="SignUpForm" method="POST">
+            <label> Name: </label> <input type ="text" name ="name"/>
+            <br />
+            <label> Username: </label> <input type ="text" name ="username"/>
+            <br />
+            <label> Password: </label><input type ="text" name ="password"/>
+            <br />
+
+            <a href ="signUpButton">
+                <input type ="submit" value ="Sign Up"/>
+            </a>
+        </form>
+        <br /> 
+        <a href ="BackButton" class = "abutton">
+            <button>Back</button>
+        </a>
+    </body>
+    </>
+  )
+}
+
+export default App
