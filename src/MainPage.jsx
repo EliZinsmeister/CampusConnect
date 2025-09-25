336af16205718f46ddaf1667037c7d5768d602d0
stevie-green <stevie-lynn_green@s.thevillageschool.com>
2025-09-25 12:32:01 -0500

    css

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import './MainPage.css'
 
 function MainPage() {
 
   return (
     <>
-      <title>Sign Up/Log In </title>
-      <link rel="stylesheet" href="css.css" />
 
-      <body>
-        <h1>Sign Up or Log In</h1>
-        <table>
-          <tr>
-            <td>
-              <a href="SignUp">
-                <button>Sign Up</button>
-              </a>
+      <div class="center">
+        <title>Sign Up/Log In </title>
+        <link rel="stylesheet" href="css.css" />
 
-              <br></br>
-            </td>
-            <td>
-              <a href="LogIn">
-                <button>Log In</button>
-              </a>
-            </td>
-          </tr>
-        </table>
-      </body>
+
+        <h1 class="center">Sign Up or Log In</h1>
+
+
+
+        <a href="SignUp" class="center">
+          <button class="center">Sign Up</button>
+        </a>
+
+        <br></br>
+
+
+        <a href="LogIn" class="center">
+          <button class="center">Log In</button>
+        </a>
+      </div>
+
     </>
   )
 }
